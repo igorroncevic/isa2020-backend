79b4ac0f7ab15eba5be7f76ef611dcc8f4aff891
zukovicfilip <zukovicfilip@gmail.com>
2021-01-19 19:41:12 +0100

    feat.4 - Added basic architecture

---
+++ src/main/java/team18/pharmacyapp/controller/PharmacyController.java
@@ -0,0 +1,29 @@
+package team18.pharmacyapp.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import team18.pharmacyapp.model.Pharmacy;
+import team18.pharmacyapp.service.interfaces.PharmacyService;
+
+import java.util.UUID;
+
+@CrossOrigin(origins = "http://localhost:8080")
+@RestController
+@RequestMapping(value = "api/pharmacies")
+public class PharmacyController {
+    private final PharmacyService pharmacyService;
+
+    @Autowired
+    public PharmacyController(PharmacyService pharmacyService) {
+        this.pharmacyService = pharmacyService;
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Pharmacy> getById(@PathVariable UUID id) {
+        Pharmacy pharmacy = pharmacyService.getById(id);
+        pharmacy.getPharmacyMedicines();
+        return new ResponseEntity<>(pharmacy, HttpStatus.OK);
+    }
+}
---
+++ src/main/java/team18/pharmacyapp/service/PharmacyServiceImpl.java
@@ -0,0 +1,30 @@
+package team18.pharmacyapp.service;
+
+import org.springframework.stereotype.Service;
+import team18.pharmacyapp.model.Pharmacy;
+import team18.pharmacyapp.repository.PharmacyRepository;
+import team18.pharmacyapp.service.interfaces.PharmacyService;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class PharmacyServiceImpl implements PharmacyService {
+
+    private final PharmacyRepository pharmacyRepository;
+
+    public PharmacyServiceImpl(PharmacyRepository pharmacyRepository) {
+        this.pharmacyRepository = pharmacyRepository;
+    }
+
+    @Override
+    public List<Pharmacy> getAll() {
+        return pharmacyRepository.findAll();
+    }
+
+    @Override
+    public Pharmacy getById(UUID id) {
+        return pharmacyRepository.findById(id).get();
+    }
+
+}
