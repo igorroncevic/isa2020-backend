34f6c897f41c8f36216170752641a542d35418f7
igorroncevic <ironcevic98@gmail.com>
2021-02-10 00:38:03 +0100

    feat.1 - Filtering no auth medicines

--- src/main/java/team18/pharmacyapp/configuration/WebSecurityConfig.java
+++ src/main/java/team18/pharmacyapp/configuration/WebSecurityConfig.java
@@ -54,7 +54,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .authorizeRequests().antMatchers("/auth/**").permitAll()
                 .antMatchers("/h2-console/**").permitAll()
                 .antMatchers("/api/patients/activate/**").permitAll()
-                //.antMatchers("/api/terms/**").permitAll()
+                .antMatchers("/api/medicines/noauth/**").permitAll()
 
                 .anyRequest().authenticated().and()
 
