0afe54432a267068976d12249ce60026f8f23856
igorroncevic <ironcevic98@gmail.com>
2021-02-11 23:45:56 +0100

    not skipping tests on travis

--- src/test/java/team18/pharmacyapp/PharmacyappApplicationTests.java
+++ src/test/java/team18/pharmacyapp/PharmacyappApplicationTests.java
@@ -1,8 +1,11 @@
 package team18.pharmacyapp;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class PharmacyappApplicationTests {
 
