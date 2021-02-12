2304720165486a6e25d349960a4d0d173903c66d
Igor Rončević <57319163+igorroncevic@users.noreply.github.com>
2021-02-12 23:41:42 +0100

    Update WebConfig.java

--- src/main/java/team18/pharmacyapp/configuration/WebConfig.java
+++ src/main/java/team18/pharmacyapp/configuration/WebConfig.java
@@ -11,6 +11,6 @@ public class WebConfig implements WebMvcConfigurer {
     // Za svrhe razvoja konfigurisemo dozvolu za CORS kako ne bismo morali @CrossOrigin anotaciju da koristimo nad svakim kontrolerom
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedHeaders("*").allowedMethods("*").allowedOrigins("*");
+        registry.addMapping("/**").allowedHeaders("*").allowedMethods("*").allowedOrigins("http://http://localhost:8080", "https://isa-pharmacy-team18-frontend.herokuapp.com");
     }
 }
